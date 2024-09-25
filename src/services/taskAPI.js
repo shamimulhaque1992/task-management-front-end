0182e7952ea0ea42340dc40f7f9d4ba38eed248a
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2024-09-25 20:47:41 +0600

    auth requeire is added

--- src/services/taskAPI.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://task-management-server-six.vercel.app/api/v1';
-
-export const fetchTasks = () => axios.get(`${BASE_URL}/task`);
-export const createTask = (task) => axios.post(`${BASE_URL}/task/create-task`, task);
-export const updateTask = (id, updates) => axios.put(`${BASE_URL}/task/${id}`, updates);
-export const deleteTask = (id) => axios.delete(`${BASE_URL}/task/${id}`);
