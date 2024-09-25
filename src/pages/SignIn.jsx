dc260c0e301b9861f1a7914d9c6296ec0e1aa081
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2024-09-25 20:18:42 +0600

    signin and signup page updated

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,10 +2,51 @@ import { SignIn, useUser } from "@clerk/clerk-react";
 
 const SignInPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <SignIn></SignIn>
-    </div>
+    <main className="h-screen flex items-center justify-center p-6">
+      <div className="container mx-auto h-full flex items-center justify-center">
+        <div className="grid w-full h-full grid-cols-1 bg-white box-anim md:grid-cols-2">
+          <div className="text-white flex items-center justify-center flex-col p-6 md:p-10">
+            <div className="my-4">
+              <div className="rounded-md mx-auto flex justify-center">
+                <img
+                  height={100}
+                  width={100}
+                  className="object-cover "
+                  // fill={true}
+                  src="/assets/images/top_Icon.png"
+                  // placeholder="blur"
+                  // blurDataURL="/assets/images/Contener.png"
+                  alt="bg-image"
+                  style={{ borderRadius: "5px" }}
+                />
+              </div>
+              {/* <h1 className="text-3xl font-semibold ">Login</h1> */}
+              <p className="mt-2 text-xl text-green-400">
+                Task Management System
+              </p>
+            </div>
+            <SignIn></SignIn>
+            <p className="mt-4 text-xs text-slate-200">
+              Khandoker Shamimul Haque @ 2024 All rights reserved
+            </p>
+          </div>
+          <div className="relative hidden md:block rounded-md">
+            <img
+              className="object-cover "
+              src="/assets/images/signIn.jpg"
+              alt="bg-image"
+              style={{ borderRadius: "5px" }}
+            />
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
 export default SignInPage;
+{
+  /* <div className="flex items-center justify-center h-screen">
+      <SignIn></SignIn>
+    </div> */
+}
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,45 @@
 import { SignUp } from "@clerk/clerk-react";
 
 const SignUpPage = () => (
-  <div className="flex items-center justify-center h-screen">
-    <SignUp></SignUp>
-  </div>
+  <main className="h-screen flex items-center justify-center p-6">
+    <div className="container mx-auto h-full flex items-center justify-center">
+      <div className="grid w-full h-full grid-cols-1 bg-white box-anim md:grid-cols-2">
+        <div className="text-white flex items-center justify-center flex-col p-6 md:p-10">
+          <div className="my-4">
+            <div className="rounded-md mx-auto flex justify-center">
+              <img
+                height={100}
+                width={100}
+                className="object-cover "
+                // fill={true}
+                src="/assets/images/top_Icon.png"
+                // placeholder="blur"
+                // blurDataURL="/assets/images/Contener.png"
+                alt="bg-image"
+                style={{ borderRadius: "5px" }}
+              />
+            </div>
+            {/* <h1 className="text-3xl font-semibold ">Login</h1> */}
+            <p className="mt-2 text-xl text-green-400">
+              Task Management System
+            </p>
+          </div>
+          <SignUp></SignUp>
+          <p className="mt-4 text-xs text-slate-200">
+            Khandoker Shamimul Haque @ 2024 All rights reserved
+          </p>
+        </div>
+        <div className="relative hidden md:block rounded-md">
+          <img
+            className="object-cover "
+            src="/assets/images/signup.jpg"
+            alt="bg-image"
+            style={{ borderRadius: "5px" }}
+          />
+        </div>
+      </div>
+    </div>
+  </main>
 );
 
 export default SignUpPage;
